d1995dd3cbe26ad099103297d9d80f2ebaa08524
chemuel <chemuelgodes@gmail.com>
2023-10-16 18:02:17 +0800

    Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'email' => 'chemuelgodes@gmail.com',
             'role' => 'Admin',
-            'password' => Hash::make('chemuelgodes13'),
+            'password' => '$2y$10$Ljo7MDbSro.TjEMzbzw5SezZ0DuvqQyNON8WPLVdI5i0qC5VasNqy',
             'created_at' => $currentDate,
         ]);
         DB::table('profiles')->insert([
