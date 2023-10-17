44b94f1063ff87f53b916426b0de4225ef553053
chemuel <chemuelgodes@gmail.com>
2023-10-17 09:41:33 +0800

    Deploying

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo --ignore-platform-reqs
-composer global config --no-plugins allow-plugins.hirak/prestissimo true
+composer global require allow-plugins.hirak/prestissimo --ignore-platform-reqs --no-plugins
 composer install --no-dev --working-dir=/var/www/html
 
 echo "generating application key..."
