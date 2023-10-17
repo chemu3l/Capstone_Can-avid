82c1305c927fcc706e14e2e67dd3e467b830f04d
chemuel <chemuelgodes@gmail.com>
2023-10-17 08:32:54 +0800

    Deploying

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,9 +24,6 @@
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link rel="stylesheet" href="{{ asset('css/menu_class.css') }}">
     <link rel="stylesheet" href="{{ asset('css/GuestCSS/news_guest.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/GuestCSS/view_news.css') }}">
-    <link rel="stylesheet" href="{{ asset('node_modules/bootstrap/dist/css/bootstrap.min.css') }}">
-
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
