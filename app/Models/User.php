7d199e75d69b165a6a7d38f6ad61c3a7d62a7965
chemuel <chemuelgodes@gmail.com>
2023-10-20 14:38:58 +0800

    MEssage

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements MustVerifyEmail
     ];
     public function profile()
     {
-        return $this->hasOne(Profile::class);
+        return $this->hasOne(app\Models\Profile::class);
     }
     // protected $profile;
 
