0658760d578e1a2d24630af757511f350cd9f0b4
chemuel <chemuelgodes@gmail.com>
2023-11-03 16:10:07 +0800

    Reassemble

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,6 @@
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card bg-transparent text-white" style="border-radius: .5rem;">
                         <div class="card-body p-5 text-center">
-
                             <div class="mb-md-5 mt-md-4 pb-5">
 
                                 <h2 class="fw-bold mb-2 text-uppercase" style="color: green">Login</h2>
