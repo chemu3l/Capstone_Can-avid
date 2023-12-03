8f3f7d8483a27876f49a1d96791a2d47e2a4e4c7
chemuel <chemuelgodes@gmail.com>
2023-12-03 21:50:00 +0800

    Fixing Bugs

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,7 +13,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-# php artisan migrate:fresh --seed --force
+php artisan migrate:fresh --seed --force
 
 php artisan storage:link --force
 
