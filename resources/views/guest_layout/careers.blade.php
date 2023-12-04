09a8f80f408d3e39a88a901866c46fc71b975704
chemuel <chemuelgodes@gmail.com>
2023-12-04 14:05:50 +0800

    Career Design

--- resources/views/guest_layout/careers.blade.php
+++ resources/views/guest_layout/careers.blade.php
@@ -8,18 +8,16 @@
         <div class="row">
             @foreach ($careers as $career)
                 <div class="col-md-3" style="margin-left: 5%; margin-top: 10px;">
-                    <div class="card mb-4" style="border-radius:5%">
-                        <div class="card-body" style="background-color: #1A5D1A; border-radius:5%; color: white;">
-                            <h3 class="card-title">{{ $career->career_position }}</h3>
-                            <h5 class="card-subtitle mb-2 text-muted">
-                                {{ date('m-d-Y', strtotime($career->career_uploaded)) }}
-                            </h5>
-                            <p class="card-text">{{ $career->career_description }}</p>
-                            <span>{{ $career->career_requirements }}</span><br>
-                            <a href="{{ route('applicants.create', ['career_id' => $career->id]) }}"
-                                style="border-color: black;" class="btn btn-success">Apply
-                                Now</a>
-                        </div>
+                    <div class="card-body-career" style="background-color: #1A5D1A; border-radius:5%; color: white;">
+                        <h3 class="card-title-career">{{ $career->career_position }}</h3>
+                        <h5 class="card-subtitle mb-2 text-muted">
+                            {{ date('m-d-Y', strtotime($career->career_uploaded)) }}
+                        </h5>
+                        <p class="card-text">{{ $career->career_description }}</p>
+                        <span>{{ $career->career_requirements }}</span><br>
+                        <a href="{{ route('applicants.create', ['career_id' => $career->id]) }}"
+                            style="border-color: black;" class="btn btn-success">Apply
+                            Now</a>
                     </div>
                 </div>
             @endforeach
