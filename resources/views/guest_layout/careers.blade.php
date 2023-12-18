00a805adddb795b0fccef2a834912b38133b8397
chemuel <chemuelgodes@gmail.com>
2023-12-18 21:00:37 +0800

    --Message--

--- resources/views/guest_layout/careers.blade.php
+++ resources/views/guest_layout/careers.blade.php
@@ -7,14 +7,14 @@
     <div class="menu_class">
         <div class="row">
             @foreach ($careers as $career)
-                <div class="col-md-3" style="margin-left: 5%; margin-top: 10px;">
+                <div class="col-md-3" style="margin-left: 5%; margin-top: 30px;">
                     <div class="card-body-career" style="background-color: #1A5D1A; border-radius:5%; color: white; padding: 10px;">
                         <h3 class="card-title-career">{{ $career->career_position }}</h3>
                         <h5 class="card-subtitle mb-2 text-muted">
-                            {{ date('m-d-Y', strtotime($career->career_uploaded)) }}
+                            {{-- {{ date('m-d-Y', strtotime($career->career_uploaded)) }} --}}
                         </h5>
-                        <p class="card-text">{{ $career->career_description }}</p>
-                        <span>{{ $career->career_requirements }}</span><br><br>
+                        <p class="card-text", style="margin-top: 20px;">{{ $career->career_description }}</p>
+                        <span>Requirements: {{ $career->career_requirements }}</span><br><br>
                         <center>
                         <a href="{{ route('applicants.create', ['career_id' => $career->id]) }}"
                             style="border-color: black;" class="btn btn-success">Apply
